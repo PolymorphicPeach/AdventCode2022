5cdc04ab7d619e07628ea83489ac07700dc37559
Matthew Peach <peach.matthew.93@gmail.com>
2022-12-03 10:47:05 -0600

    improve solution

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -4,16 +4,37 @@
 #include <vector>
 #include <algorithm>
 #include <set>
+#include <thread>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::vector;
 
+static int threadsLaunched{0};
+void part1();
+void part2();
+
 int main(){
+	
+
+	std::thread t1(part1);
+	std::thread t2(part2);
+
+	t1.join();
+	t2.join();
+
+	cout << "Threads launched = " << threadsLaunched << "\n\n";
+
+	return 0;
+}
+
+void part1(){
 	std::ifstream data;
 	data.open("aoc3.txt", std::ios::in);
 	string line;
+	int sumItems{0};
+	int asciiConvert;
 
 	string compartment1;
 	string compartment2;
@@ -23,9 +44,6 @@ int main(){
 	if(data.fail()){
 		std::cerr << "Error: File not found \n";
 	}
-
-
-
 
 	while(!data.eof()){
 		getline(data, line);
@@ -38,7 +56,7 @@ int main(){
 		for(size_t i{0}; i < compartment1.size() && !match; ++i){
 
 			for(size_t j{0}; j < compartment2.size() && !match; ++j){
-				
+
 				if(compartment1[i] == compartment2[j]){
 					commonItems += compartment1[i];
 					match = true;
@@ -48,143 +66,140 @@ int main(){
 
 		}
 
-
 	}
-
-	int value{0};
-	int asciiConvert;
 
 	for(int i{0}; i < commonItems.size(); ++i){
 		if(std::isupper(commonItems[i])){
 			asciiConvert = commonItems[i] - 38;
-			value += asciiConvert;
+			sumItems += asciiConvert;
 		}
 		else{
 			asciiConvert = commonItems[i] - 96;
-			value += asciiConvert;
+			sumItems += asciiConvert;
 
 		}
 
 
 	}
 
-	cout << value << "\n\n";
+	cout << "Part 1 Solution: " << sumItems << "\n\n";
 
 	data.close();
-	data.open("aoc3.txt", std::ios::in);
+}
 
+void part2(){
+	std::ifstream data;
+	data.open("aoc3.txt", std::ios::in);
 
 	int sumOfGroups{0};
 	string group1Stuff;
 	string group2Stuff;
+	string line;
 
 	while(!data.eof()){
-
-
 		vector <string> group1;
-
 		vector <string> group2;
 
 		for(int i{0}; i < 3; ++i){
 			getline(data, line);
 			group1.push_back(line);
-			
 		}
 
 		for(int i{0}; i < 3; ++i){
 			getline(data, line);
 			group2.push_back(line);
-
-
 		}
 
-		string common1;
-		string common2;
+		char thread1ReturnChar;
+		char thread2ReturnChar;
 
-		string common1Pt2;
-		string common2Pt2;
+		std::thread group1Thread([&group1, &thread1ReturnChar](){
+			++threadsLaunched;
+			string commonPt1;
+			string commonPt2;
 
-		for(size_t i{0}; i < group1.size(); ++i){
-			std::sort(group1[i].begin(), group1[i].end());
-			std::sort(group2[i].begin(), group2[i].end());
-		}
-
-
-		std::set_intersection(group1[0].begin(), group1[0].end(),
-			group1[1].begin() , group1[1].end(), std::back_inserter(common1));
-
-		std::set_intersection(group2[0].begin(), group2[0].end(),
-			group2[1].begin(), group2[1].end(), std::back_inserter(common2));
-
-		std::sort(common1.begin(), common1.end());
-		std::sort(common2.begin(), common2.end());
-
-		std::set_intersection(common1.begin(), common1.end(),
-			group1[2].begin(), group1[2].end(), std::back_inserter(common1Pt2));
-
-		std::set_intersection(common2.begin(), common2.end(),
-			group2[2].begin(), group2[2].end(), std::back_inserter(common2Pt2));
-
-
-
-		if(common1Pt2.size() > 1){
-			cout << "ALERT pt 1 GREATER THAN 1" << std::endl;
 			for(size_t i{0}; i < group1.size(); ++i){
-				cout << group1[i] << std::endl;
+				std::sort(group1[i].begin(), group1[i].end());
 			}
-			cout << common1Pt2 << std::endl;
-			cout << std::endl;
-		}
-		if(common2Pt2.size() > 1){
-			cout << "ALERT pt 2 GREATER THAN 1" << std::endl;
+
+			std::set_intersection(group1[0].begin(), group1[0].end(),
+				group1[1].begin(), group1[1].end(), std::back_inserter(commonPt1));
+
+			std::sort(commonPt1.begin(), commonPt1.end());
+
+			std::set_intersection(commonPt1.begin(), commonPt1.end(),
+				group1[2].begin(), group1[2].end(), std::back_inserter(commonPt2));
+
+			thread1ReturnChar = commonPt2[0];
+
+		});
+
+		std::thread group2Thread([&group2, &thread2ReturnChar](){
+			++threadsLaunched;
+			string commonPt1;
+			string commonPt2;
+
 			for(size_t i{0}; i < group2.size(); ++i){
-				cout << group2[i] << std::endl;
+				std::sort(group2[i].begin(), group2[i].end());
 			}
-			cout << common2Pt2 << std::endl;
-			cout << std::endl;
-		}
 
-		group1Stuff += common1Pt2[0];
-		group2Stuff += common2Pt2[0];
+			std::set_intersection(group2[0].begin(), group2[0].end(),
+				group2[1].begin(), group2[1].end(), std::back_inserter(commonPt1));
+
+			std::sort(commonPt1.begin(), commonPt1.end());
+
+
+
+			std::set_intersection(commonPt1.begin(), commonPt1.end(),
+				group2[2].begin(), group2[2].end(), std::back_inserter(commonPt2));
+
+			thread2ReturnChar = commonPt2[0];
+		});
+
+		group1Thread.join();
+		group2Thread.join();
+
+		group1Stuff += thread1ReturnChar;
+		group2Stuff += thread2ReturnChar;
 	}
 
-	int value2{0};
-	asciiConvert = 0;
+	std::thread group1Thread([&group1Stuff, &sumOfGroups](){
+		++threadsLaunched;
+		int asciiConvert;
 
-	cout << group1Stuff << "\n" << group2Stuff << "\n\n";
+		for(int i{0}; i < group1Stuff.size(); ++i){
 
-	for(int i{0}; i < group1Stuff.size(); ++i){
+			if(std::isupper(group1Stuff[i])){
+				asciiConvert = group1Stuff[i] - 38;
+				sumOfGroups += asciiConvert;
+			}
+			else{
+				asciiConvert = group1Stuff[i] - 96;
+				sumOfGroups += asciiConvert;
 
-
-
-		if(std::isupper(group1Stuff[i])){
-			asciiConvert = group1Stuff[i] - 38;
-			value2 += asciiConvert;
+			}
 		}
-		else{
-			asciiConvert = group1Stuff[i] - 96;
-			value2 += asciiConvert;
+	});
 
+	std::thread group2Thread([&group2Stuff, &sumOfGroups](){
+		++threadsLaunched;
+		int asciiConvert;
+
+		for(int i{0}; i < group2Stuff.size(); ++i){
+			if(std::isupper(group2Stuff[i])){
+				asciiConvert = group2Stuff[i] - 38;
+				sumOfGroups += asciiConvert;
+			}
+			else{
+				asciiConvert = group2Stuff[i] - 96;
+				sumOfGroups += asciiConvert;
+
+			}
 		}
-	}
+	});
 
-	for(int i{0}; i < group2Stuff.size(); ++i){
-		if(std::isupper(group2Stuff[i])){
-			asciiConvert = group2Stuff[i] - 38;
-			value2 += asciiConvert;
-		}
-		else{
-			asciiConvert = group2Stuff[i] - 96;
-			value2 += asciiConvert;
+	group1Thread.join();
+	group2Thread.join();
 
-		}
-	}
-
-	cout << "g1 size = " << group1Stuff.size() << "\n";
-	cout << "g2 size = " << group2Stuff.size() << "\n";
-
-	cout << value2 << "\n\n";
-
-
-	return 0;
+	cout << "Part 2 Solution: " << sumOfGroups << "\n\n";
 }
